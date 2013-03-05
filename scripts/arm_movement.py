2bf244f7e1a27ba9c224339b092db403ad23b880
Alessandro Farsi <af342@robotics31.(none)>
2013-03-05 15:04:49 -0500

    Improved arm_movement now it can also read current arm positions add grip action

--- scripts/arm_movement.py
+++ scripts/arm_movement.py
@@ -6,67 +6,102 @@ sentrybot
 
 import roslib; roslib.load_manifest('ros_sentry')
 import rospy
+import tf
+
+import actionlib
+
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from pr2_controllers_msgs.msg import JointTrajectoryAction, JointTrajectoryGoal
 from trajectory_msgs.msg import JointTrajectoryPoint
-import actionlib
 
-def callback(data):
-    rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
+from pr2_controllers_msgs.msg import Pr2GripperCommandAction, Pr2GripperCommandGoal
+    
 
+SH_CONTROLLERS = {
+    'r': '/r_arm_controller/joints',
+    'l': '/l_arm_controller/joints',
+}
 
-def listener():
-    rospy.init_node('mastermind', anonymous=True)
-    l_client = actionlib.SimpleActionClient('r_arm_controller/joint_trajectory_action', JointTrajectoryAction)
-    l_client.wait_for_server()
+def create_goal(joints_p, arm_side = 'r'):
+    """ Create a goal for a given set of joints angles
 
-    print l_client
-    # Goal
+    joints_p is a vector of angles
+    """
     goal = JointTrajectoryGoal()
     # Populates trajectory with joint names.
-    goal.trajectory.joint_names.append("r_shoulder_pan_joint")
-    goal.trajectory.joint_names.append("r_shoulder_lift_joint")
-    goal.trajectory.joint_names.append("r_upper_arm_roll_joint")
-    goal.trajectory.joint_names.append("r_elbow_flex_joint")
-    goal.trajectory.joint_names.append("r_forearm_roll_joint")
-    goal.trajectory.joint_names.append("r_wrist_flex_joint")
-    goal.trajectory.joint_names.append("r_wrist_roll_joint")
+    for joint_name in rospy.get_param(SH_CONTROLLERS[arm_side]):
+        goal.trajectory.joint_names.append(joint_name)
     
-    # First trajectory point
+    goal.trajectory.points = []
+
     # Positions
-    ind = 0
-    #print goal.trajectory.points
-    point1 = JointTrajectoryPoint()
-    point2 = JointTrajectoryPoint()
-    goal.trajectory.points = [point1, point2]
-    #goal.trajectory.points[ind].positions.resize(7)
-    point1.positions = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    point1.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    goal.trajectory.points[ind] = point1
+    for ind in xrange(len(joints_p)):
+        point = JointTrajectoryPoint()
+
+        point.positions = joints_p[ind]
+        point.velocities = [0]*len(point.positions)
+        goal.trajectory.points.append(point)
+        #To be reached 1 second after starting along the trajectory
+        goal.trajectory.points[ind].time_from_start = rospy.Duration(1.5)*ind
+
+    goal.trajectory.header.stamp = rospy.Time.now()+rospy.Duration(.5)        
+    return goal
+
+
+## Encode wave
+wave = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+        [0.1, 0.3,  0.0, -1.8,  0.0,  0.0,   0.3],
+        [0.1, 0.2, 0.0, -0.8, 0.0, 0.0, 0.0],
+        [0.1, 0.3,  0.0, -1.8,  0.0,  0.0,   0.3],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+
+
+def getArmPos(arm_side = 'r'):
+    tf_listener.waitForTransform('/' + arm_side + '_wrist_roll_link', '/base_footprint', rospy.Time(0),rospy.Time(3))
+    (trans,rot) = tf_listener.lookupTransform('/' + arm_side + '_wrist_roll_link', '/base_footprint', rospy.Time(0))
+    return trans, rot
+
+def moveGrip(p = 0.08, arm_side = 'r'):
+    open = Pr2GripperCommandGoal()
+    open.command.position = p
+    open.command.max_effort = -1.0
+    r_g_controller.send_goal(open)
     
-    #To be reached 1 second after starting along the trajectory
-    goal.trajectory.points[ind].time_from_start = rospy.Duration(1.0)
 
-    # Second trajectory point
-    # Positions
-    ind += 1
-    point1.positions = [-0.3,0.2,-0.1,-1.2,1.5,-0.3,0.5]
-    point1.velocities = [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-    goal.trajectory.points[ind] = point1
-    goal.trajectory.points[ind].time_from_start = rospy.Duration(2.0)
+def waver():
+    l_controller.send_goal(create_goal(wave, 'l'))
+    #l_client.send_goal(create_goal([-0.3,0.2,-0.1,-1.2,1.5,-0.3,0.5]))
 
-    goal.trajectory.header.stamp = rospy.Time.now()+rospy.Duration(1.0)        
-    l_client.send_goal(goal)
+    # Goal
+   
 
     #rospy.Subscriber("chatter", String, callback)
     
-    rospy.spin()
+    #rospy.spin()
+
 
 
 if __name__ == '__main__':
+    rospy.init_node('arm_movement', anonymous=True)
+    ## Init servers
+    l_controller = actionlib.SimpleActionClient('l_arm_controller/joint_trajectory_action', JointTrajectoryAction)
+    l_controller.wait_for_server()
+    r_controller = actionlib.SimpleActionClient('r_arm_controller/joint_trajectory_action', JointTrajectoryAction)
+    r_controller.wait_for_server()
+    r_g_controller = actionlib.SimpleActionClient('r_gripper_controller/gripper_action', Pr2GripperCommandAction)
+    r_g_controller.wait_for_server()
+
+    tf_listener = tf.TransformListener()
+
     ## Get List of Arms Joints
-    print rospy.get_param('/r_arm_controller/joints')
+    print rospy.get_param('/r_gripper_controller/joint')
 
-
-    listener()
+    print getArmPos(arm_side = 'r')
+    rospy.loginfo(rospy.get_name() + ": I'm waving to you")
+    waver()
+    moveGrip(0.08)
+    r_g_controller.wait_for_result()
+    moveGrip(0)
+    print getArmPos(arm_side = 'r')
+    
