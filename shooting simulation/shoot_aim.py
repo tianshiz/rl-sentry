730cbb6202c81148d9dd704c7c08182d2f5cd919
Vedanix <raku@raku-A15.(none)>
2013-03-24 23:07:09 -0400

    updated shooting code so that its object oriented. simply import shoot_aim.py in your code. to create a bullet and shoot it: bullet1=shoot_aim(xpos,ypos,zpos,speed,bulletNum)

--- shooting simulation/shoot_aim.py
+++ shooting simulation/shoot_aim.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#how to run this testcode: python shoot_aim.py .79 -.188 .9 bullet30 0.4
+# shoots bullet in x axis direction with initial pt[.79,-.188,.9]. bullet30 is the
+#name of the bullet, can be anything, but must be unique for each bullet!
+#0.4 is the speed
+#iMPORTANT: in actualy application, just import this python file and create new
+#instances like so: bullet1=shoot_aim(xpos,ypos,zpos,speed,bulletNum)
 import roslib
 roslib.load_manifest('gazebo')
 import rospy,os,sys
@@ -9,31 +15,37 @@ from gazebo_msgs.msg import *
 from gazebo_msgs.srv import *
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Wrench
 import tf.transformations as tft
-import spawn_model
+
+
+class shoot_aim():
+    def __init__(self,x,y,z,speed,name):
+        point= Point()
+        point.x=x
+        point.y=y
+        point.z=z
+        self.initial=point
+        self.speed=speed
+        self.name=name
+        bulletNum=name
+
+        spawn(x,y,z,bulletNum)
+        shoot_server(bulletNum+'::my_box',speed,point)
 
 
 #inserts model at exact place on right wrist
 def spawn(xpos,ypos,zpos,bulletNum):
-	#os.system('python '+ direc+' -x '+xpos+' -y '+ypos+' -z '+zpos+' -model '+bulletNum)
-	#return 0
-    sm=spawn_model.SpawnModel()
-    sm.inital_xyz=[xpos,ypos,zpos]
-    sm.file_name='bullet.urdf'
-    sm.model_name=bulletNum
-    sm.urdf_format=True
-    sm.callSpawnService()
-    return
-         
+    os.system('python '+ direc+' -x '+str(xpos)+' -y '+str(ypos)+' -z '+str(zpos)+' -model '+bulletNum)
+
+#calls services to apply wrench, we only care about force in x direction. 
 def shoot_server(bullet_name,xforce,point):
 
     apply_wrench_server = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
-    print 'server started'
     wrench=Wrench()
     wrench.force.x=xforce
     
     start_time=rospy.Time(10)
 
-    duration=rospy.Duration(10)
+    duration=rospy.Duration(1)
     try:
       resp1 = apply_wrench_server(bullet_name,'',point,wrench,start_time,duration)
     except rospy.ServiceException, e:
@@ -41,23 +53,20 @@ def shoot_server(bullet_name,xforce,point):
 
 
 if __name__ == "__main__":
-	#xpos=sys.argv[0]
-	#ypos=sys.argv[1]
-	#zpos=sys.argv[2]
-	#bulletNum=sys.argv[3]
+    xpos=float(sys.argv[1])
+    ypos=float(sys.argv[2])
+    zpos=float(sys.argv[3])
+    bulletNum=sys.argv[4] #name given to bullet, any string
+    speed=float(sys.argv[5]) #speed in m/s
     
-	xpos='.7697'
-	ypos='-.188'
-	zpos='.9'
-	point= Point()
-	point.x=.7697
-	point.y=-.188
-	point.z=.9
+    #xpos=.7697
+    #ypos=-.188
+    #zpos=.9
+    #create new instance of bullet easily like this
+    bullet1=shoot_aim(xpos,ypos,zpos,speed,bulletNum)
 
-	bulletNum='bullet12'
-	spawn(xpos,ypos,zpos,bulletNum)
-	shoot_server('bullet12::my_box',0.4,point)
-	#apply_wrench_server()
 
-	
-	
+
+
+    
+    
